179858eeea5c1a532ff93ee578ed23f55ef2c083
CreativeNamingSense <152135308+CreativeNamingSense@users.noreply.github.com>
2024-02-27 06:00:15 +0000

    modified:   2024/02/26 try & except .py

--- 2024/02/26 try & except .py
+++ 2024/02/26 try & except .py
@@ -1,9 +1,17 @@
-num1 = int(input("Please input the first number: "))
-num2 = int(input("Please input the second number: "))
+# num1 = int(input("Please input the first number: "))
+# num2 = int(input("Please input the second number: "))
 
+# try:
+#     print(f"The value is: {num1/num2}")
+# except ZeroDivisionError:
+#     print("∞")
+# except TypeError:
+#     print("Please input an appropriate integer value.")
+
+
+list = [None for i in range(0,10)]
+print(list)
 try:
-    print(f"The value is: {num1/num2}")
-except ZeroDivisionError:
-    print("∞")
-except TypeError:
-    print("Please input an appropriate integer value.")
+    list[0] = int(input("Enter a value: "))
+except IndexError:
+    print("out of range.")
