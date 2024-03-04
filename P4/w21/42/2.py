004a072c11272a0d9f09934f43ee6d9afad21b7d
CreativeNamingSense <152135308+CreativeNamingSense@users.noreply.github.com>
2024-03-04 07:47:01 +0000

    .

--- P4/w21/42/2.py
+++ P4/w21/42/2.py
@@ -9,18 +9,58 @@ class Picture:
     def GetDescription(self):
        return self.__description
     def GetHeight(self):
-        return self.__height
+        return int(self.__height)
     def GetWidth(self):
-        return self.__width
+        return int(self.__width)
     def GetColour(self):
         return self.__frameColour
     
     def setDescription (self, pDescription):
         self.__description = pDescription
+
+
+def ReadData(myPictures, fileName):
+    counter = 0
+    try:
+        file = open(fileName, "r")
+        pdescription = file.readline().strip()
         
+        while pdescription != "":            
+            pwidth = file.readline().strip()
+            pheight = file.readline().strip()
+            pframeColour = file.readline().strip()
+            myPictures[counter] = Picture(pdescription, pwidth, pheight, pframeColour)
+            counter = counter + 1
+            pdescription = file.readline().strip()
+        file.close()    
+    except IOError:
+        print("The file was not found.")    
+    return counter, myPictures
+
+
+fileName = "Pictures.txt"        
 myPictures = [] 
 
 for i in range(100):
     myPictures.append(Picture("",0,0,""))
 
-print(myPictures)
+counter, myPictures = ReadData(myPictures, fileName)
+
+picColour = str(input("Please input the colour of the picture:"))
+picHeight = int(input("Please input the height of the picture:"))
+picWidth = int(input("Please input the width of the picture: "))
+
+index = 0
+for index in range(counter):
+    if picColour.lower() == (myPictures[index].GetColour()).lower() and picHeight >= myPictures[index].GetHeight() and picWidth >= myPictures[index].GetWidth():
+        print(myPictures[index].GetDescription())
+        print(myPictures[index].GetWidth())
+        print(myPictures[index].GetHeight())
+    index = index + 1   
+    
+        
+
+    
+
+
+
